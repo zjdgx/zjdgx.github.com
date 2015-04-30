19ec17599bc7d56ef314dd801f54bde395ab37c9
zjdgx <zjdgx20131030@163.com>
2015-04-30 09:48:34 +0800

    TODO list

---
+++ source/微网站模版/Baodi/njs/Page.App.js
@@ -0,0 +1,18 @@
+﻿
+function windowResize() {
+	
+	var bodyHeight = $(window).height();
+    var midHeight = $(".otherbox").outerHeight();
+    var copyHeight = $(".copyright").outerHeight();
+	var footHeight = $(".menwrap").outerHeight();
+	
+	$(".yijiantop").outerHeight((bodyHeight - midHeight - copyHeight-footHeight)/2 );
+	$(".yijianbot").outerHeight((bodyHeight - midHeight - copyHeight-footHeight)/2 + 40);
+    
+	
+}
+
+windowResize();
+$(window).resize(function () {
+    windowResize();
+});
