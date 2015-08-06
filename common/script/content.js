b70827fa9fe23f61eb013240deb81c9ffc92ead5
zdjgx <zjdgx20131030@163.com>
2015-08-06 22:53:33 +0800

    redesign home page as blog

---
+++ common/script/content.js
@@ -0,0 +1,62 @@
+﻿window.zjdgxIssues = {};
+
+window.zjdgxIssues.memo = [
+	{
+		date: '2015-07-16',
+		text: '手机上网站导航的制作',
+		href: 'http://www.w3cplus.com/jquery/mobile-navigation-design-with-jquery'
+	},
+	{
+		date: '2015-07-16',
+		text:'玩转HTML5移动页面（动效篇）',
+		href: 'http://www.websjy.com/article-2789-1.html'
+	}
+];
+
+window.zjdgxIssues.type = {
+	'javascript': 'javascript',
+	'css': 'css',
+	'html5': 'html5',
+	'nodejs': 'nodejs',
+	'构建工具': 'buildTool'
+};
+
+window.zjdgxIssues.issues = [
+	{
+		type: 'buildTool',
+		date: '2015-08-06',
+		text: 'gulp requirejs配置',
+		href: '/tool/gulp-require.html',
+		description: '本文主要演示requirejs在gulp中的配置，以及与grunt下的配置的区别, gulp采用gulp-replace来替换js文件中的一些参数....'
+	},
+	{
+		type: 'buildTool',
+		date: '2015-08-06',
+		text: 'gulp requirejs配置',
+		href: '/tool/gulp-require.html'
+	},
+	{
+		type: 'javascript',
+		date: '2015-08-06',
+		text: 'gulp requirejs配置',
+		href: '/tool/gulp-require.html'
+	},
+	{
+		type: 'buildTool',
+		date: '2015-08-06',
+		text: 'gulp requirejs配置',
+		href: '/tool/gulp-require.html'
+	},
+	{
+		type: 'nodejs',
+		date: '2015-07-06',
+		text: 'gulp requirejs配置',
+		href: '/tool/gulp-require.html'
+	},
+	{
+		type: 'html5',
+		date: '2015-06-06',
+		text: 'html5',
+		href: '/tool/gulp-require.html'
+	}
+];
