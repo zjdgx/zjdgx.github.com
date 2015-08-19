1d9ed7d4a2e5d9af810c4a9423c63658a9d60284
zjdgx <zjdgx20131030@163.com>
2015-08-19 17:59:26 +0800

    great engineer

--- common/script/content.js
+++ common/script/content.js
@@ -23,18 +23,20 @@ window.zjdgxIssues.type = {
 
 window.zjdgxIssues.issues = [
 	{
+		type: 'notes',
+		date: '2015-08-19',
+		text: '如何成为一名卓越的前端工程师',
+		href: 'blog/notes/how-to-become-a-greate-front-end-engineer.html',
+		description: '看过之后非常有感触，很多观点都是自己长期非常坚持和认同的，所以翻译出来分享给更多的前端同学！'
+	},
+	{
 		type: 'buildTool',
 		date: '2015-08-06',
 		text: 'gulp requirejs配置',
 		href: 'blog/tool/gulp-require.html',
 		description: '本文主要演示requirejs在gulp中的配置，以及与grunt下的配置的区别, gulp采用gulp-replace来替换js文件中的一些参数，然后用concat把配置文件和requirejs文件合并；grunt直接在从concat中配置require参数，然后合并....'
 	},
-	{
-		type: 'buildTool',
-		date: '2015-08-06',
-		text: 'gulp requirejs配置',
-		href: '/tool/gulp-require.html'
-	},
+
 	{
 		type: 'javascript',
 		date: '2015-08-06',
