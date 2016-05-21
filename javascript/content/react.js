de79fa4ed36fc4132e37b9315141d064cac18f7d
zdjgx <zjdgx20131030@163.com>
2016-05-21 08:27:59 +0800

    react canvas avatar

--- javascript/content/react.js
+++ javascript/content/react.js
@@ -8,6 +8,12 @@
 define(['Backbone'], function (Backbone) {
 	return Backbone.Collection.extend({
 		content: [
+			{
+				title: 'react canvas avatar',
+				date: '2016-05-21',
+				url: 'notes/react/canvasUpload.html',
+				des: 'react canvas头像上传'
+			}
 		]
 	});
 });
