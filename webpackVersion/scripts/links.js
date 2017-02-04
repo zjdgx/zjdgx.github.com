4681e5fb1d81b4f7bd6b7cba0eee892c103cc368
zjdgx <zjdgx20131030@163.com>
2017-02-04 17:07:48 +0800

    add source link on home page

---
+++ webpackVersion/scripts/links.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    url: 'http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html',
+    title: 'flex布局教程',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/AlloyTeam/Mars/tree/master/tools',
+    title: '腾讯mars工具',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/sindresorhus/awesome-nodejs',
+    title: 'awesome nodejs',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/hawx1993/github-FE-project',
+    title: '前端资源',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/sorrycc/awesome-javascript',
+    title: 'awesome javascript',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/sindresorhus/awesome#front-end-development',
+    title: 'awesome',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/vhf/free-programming-books/blob/master/free-programming-books-zh.md',
+    title: '免费开发书籍',
+    date: '2017-02-04'
+  },
+  {
+    url: 'https://github.com/qinjx/30min_guides/blob/master/shell.md',
+    title: 'Shell脚本编程30分钟入门',
+    date: '2017-02-04'
+  }
+];
