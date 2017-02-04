ee9aa7f352e9ef7efd1ecedbd245ab2751a79bae
zjdgx <zjdgx20131030@163.com>
2017-02-04 17:07:16 +0800

    add source link on home page

--- webpackVersion/scripts/app.js
+++ webpackVersion/scripts/app.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import Contents from './content/navContent';
 import NavList from './components/OneNoteNav';
+import Links from './links';
 
 export default class APPView extends React.Component {
   constructor() {
@@ -22,6 +23,11 @@ export default class APPView extends React.Component {
   };
 
   render() {
+    console.log(Links);
+    const linkHtml = Links.map((link, index) => {
+      return <a key={index} href={link.url} target='_blank' title={link.title}>{link.title}</a>;
+    });
+
     return (
       <div className='content'>
         <NavList setContentUrl={this.setContentUrl.bind(this)} updateStyle={() => this.setState({update: new Date().getTime()})} NavList={Contents}/>
@@ -32,7 +38,8 @@ export default class APPView extends React.Component {
                              marginWidth="0"></iframe>
             } else {
               return <div className='layout-center'>
-                <img src='static/image/china-map.png'/>
+                <div className='useful-links'>{linkHtml}</div>
+                <img className='china-map' src='static/image/china-map.png'/>
               </div>
             }
           }.bind(this))()
