ace7f1024005f0aae535d4e1c698368a337feb53
zjdgx <zjdgx20131030@163.com>
2017-07-29 07:57:42 +0800

    add lottery and geolocation

--- webpackVersion/scripts/content/html.js
+++ webpackVersion/scripts/content/html.js
@@ -1,10 +1,5 @@
 module.exports = {
 	title: 'HTML',
 	contents: [
-		{
-			title: 'Canvas抽奖游戏',
-			date: '2016-03-21',
-			url: 'static/notes/html/lottery.html'
-		}
 	]
 };
--- webpackVersion/scripts/content/opus.js
+++ webpackVersion/scripts/content/opus.js
@@ -1,11 +1,23 @@
 module.exports = {
 	title: 'My Opus',
 	contents: [
+    {
+      title: 'H5 Geolocation',
+      date: '2017-02-24',
+      url: 'static/notes/opus/geolocation/index.html',
+      des: 'H5 Geolocation'
+    },
 		{
 			title: '专注力测试',
 			date: '2016-10-01',
 			url: 'static/notes/opus/concentration/index.html',
 			des: '专注力测试'
-		}
+		},
+    {
+      title: 'Lottery',
+      date: '2013-11-29',
+      url: 'static/notes/opus/lottery/lottery.html',
+      des: 'Lottery'
+    }
 	]
 };
